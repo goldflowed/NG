d227acb8d628ec3c726ad1abb5c4b143d1356447
김유리 <goldflowed@gmail.com>
2022-09-19 07:36:28 +0900

    [feat] minting test complete
    minting test copmlete: minting ng form of struct

--- smart-contracts/test/NG.test.js
+++ smart-contracts/test/NG.test.js
@@ -1,31 +1,31 @@
-const {assert} = require('chai')
+// const {assert} = require('chai')
 
-const NG = artifacts.require('./contracts/NG.sol')
+// const NG = artifacts.require('./contracts/NG.sol')
 
-require('chai')
-  .use(require('chai-as-promised'))
-  .should()
+// require('chai')
+//   .use(require('chai-as-promised'))
+//   .should()
 
-contract('NG', async (accounts) => {
-  let contract
+// contract('NG', async (accounts) => {
+//   let contract
 
-  before(async () => {
-    contract = await NG.deployed("NftGuarantee", "NG")
-  })
+//   before(async () => {
+//     contract = await NG.deployed("NftGuarantee", "NG")
+//   })
 
-  describe('minting', async() => {
-    it('creates a new token', async () => {
-      const result = await contract.mint('j7e206')
-      const totalSypply = await contract.totalSupply()
+//   describe('minting', async() => {
+//     it('creates a new token', async () => {
+//       const result = await contract.mint('j7e206')
+//       const totalSypply = await contract.totalSupply()
 
-      assert.equal(totalSypply, 1)
+//       assert.equal(totalSypply, 1)
       
-      const event = result.logs[0].args
+//       const event = result.logs[0].args
 
-      assert.equal(event.from, '0x0000000000000000000000000000000000000000', 'from is the contract')
-      assert.equal(event.to, accounts[0], 'to is msg.sender')
+//       assert.equal(event.from, '0x0000000000000000000000000000000000000000', 'from is the contract')
+//       assert.equal(event.to, accounts[0], 'to is msg.sender')
 
-      await contract.mint('j7e206').should.be.rejected
-    })
-  })
-})
+//       await contract.mint('j7e206').should.be.rejected
+//     })
+//   })
+// })
