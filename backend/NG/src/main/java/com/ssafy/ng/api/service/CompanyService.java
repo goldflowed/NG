a6c4d65d5b90ed23f6015c7561647c6022795385
KimDaeyeong <daeyoung4088@hanmail.net>
2022-09-19 13:53:51 +0900

    [fix] ì기업 / 어드민 코드 통합

--- backend/NG/src/main/java/com/ssafy/ng/api/service/CompanyService.java
+++ backend/NG/src/main/java/com/ssafy/ng/api/service/CompanyService.java
@@ -1,7 +1,9 @@
 package com.ssafy.ng.api.service;
 
+import com.ssafy.ng.api.request.CompanyPermitReq;
 import com.ssafy.ng.api.request.CompanyPostReq;
 import com.ssafy.ng.api.response.CompanyGetRes;
+import com.ssafy.ng.common.customObject.CompanyList;
 import com.ssafy.ng.db.entity.Company;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -15,4 +17,12 @@ public interface CompanyService {
 
     // 기업정보 조회하기
     CompanyGetRes getCompanyByComWallet(String comWallet);
+
+    // admin============================================================================================================
+
+    // 승인 요청한 기업 리스트
+    Page<CompanyList> comList(Pageable pageable);
+
+    // 기업 승인
+    boolean permitCompany(String comWallet, CompanyPermitReq permitReq);
 }
--- backend/NG/src/main/java/com/ssafy/ng/api/service/CompanyServiceImpl.java
+++ backend/NG/src/main/java/com/ssafy/ng/api/service/CompanyServiceImpl.java
@@ -1,10 +1,15 @@
 package com.ssafy.ng.api.service;
 
+import com.ssafy.ng.api.request.CompanyPermitReq;
 import com.ssafy.ng.api.request.CompanyPostReq;
 import com.ssafy.ng.api.response.CompanyGetRes;
+import com.ssafy.ng.common.customObject.CompanyList;
 import com.ssafy.ng.db.entity.Company;
 import com.ssafy.ng.db.repository.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -40,4 +45,24 @@ public class CompanyServiceImpl implements CompanyService{
         return res;
     }
 
+    @Override
+    public Page<CompanyList> comList(Pageable pageable) {
+        PageRequest pageRequest = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize());
+        Page<Company> page = companyRepository.findAll(pageRequest);
+        Page<CompanyList> dtoPage = page
+                .map(m -> CompanyList.of(
+                        m.getComName()
+                ));
+        return dtoPage;
+    }
+
+    @Override
+    public boolean permitCompany(String comWallet, CompanyPermitReq permitReq) {
+        Optional<Company> company = companyRepository.getByComWallet(comWallet);
+        if (!company.isPresent()) {
+            return false;
+        }
+        companyRepository.permitCompany(comWallet, permitReq.getComPermit());
+        return true;
+    }
 }
