f78cbdad16f9781f54ee7bd674590edca997731d
KimDaeyeong <daeyoung4088@hanmail.net>
2022-09-26 09:46:26 +0900

    [fix] CORS세팅 수정

--- backend/NG/src/main/java/com/ssafy/ng/config/WebMvcConfig.java
+++ backend/NG/src/main/java/com/ssafy/ng/config/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package com.ssafy.ng.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -19,4 +20,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
     				.addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedOrigins("http://localhost:3000");
+	}
 }
