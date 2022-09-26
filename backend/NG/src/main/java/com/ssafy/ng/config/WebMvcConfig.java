18a44e2b169f21b1c47627ec325fe570a7f5e1a0
KimDaeyeong <daeyoung4088@hanmail.net>
2022-09-26 10:53:46 +0900

    [fix] port 추가

--- backend/NG/src/main/java/com/ssafy/ng/config/WebMvcConfig.java
+++ backend/NG/src/main/java/com/ssafy/ng/config/WebMvcConfig.java
@@ -23,6 +23,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-				.allowedOrigins("http://localhost:3000");
+				.allowedOrigins("http://localhost:3000", "https://j7e206.p.ssafy.io");
 	}
 }
