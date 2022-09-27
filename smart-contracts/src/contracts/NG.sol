d53eabc40b0241549422383f6455c5cf989d20b5
서광석 <wkdrpddo@naver.com>
2022-09-27 15:13:56 +0900

    [feat] modified data managing struct and add function
    데이터 관리 구조 변경 및 함수 추가
    ----------------------------------
    mint 함수 수정
    ----------------------------------
    예외 처리 추가
    Close
    S07P22E206-211
    S07P22E206-113

--- smart-contracts/src/contracts/NG.sol
+++ smart-contracts/src/contracts/NG.sol
@@ -5,19 +5,32 @@ import "./tokens/ERC721/ERC721Connector.sol";
 
 contract NG is ERC721Connector {
     // struct로 nft 정보 저장해서 해당 struct 배열로 nft 관리하기
+    // 인증서 각각의 정보
     struct NGInfo {
-        string brandNm;
-        string productNo;
         string serialNo;
-        string mfd;
-        string madeIn;
+        Product product;
     }
 
+    // 브랜드별 품목에 대한 정보를 담는 struct
+    struct Product {
+        string brandNm;
+        string productName;
+        string productNo;
+        string mfd;
+        string madeIn;
+        string price;
+    }
+
+    // 브랜드가 등록한 품목 리스트
+    mapping(address => Product[]) private brandToProduct;
+    // 브랜드가 등록한 품목의 index
+    mapping(address => mapping(string => uint256)) private ProductToIndex;
+    // 브랜드가 등록한 품목의 serial number당 인증서 목록
+    mapping(address => mapping(uint256 => NGInfo[])) private ProductToNg;
+    // tokenId 관리를 위한 전체 ngs 목록
     NGInfo[] public ngs;
-    // string[] public ngs;
 
     uint256[] public blockNos;
-    // string[] public txnHashes;
 
     // minted account address => token ids
     mapping(address => uint256[]) private _mintedTokens;
@@ -33,6 +46,44 @@ contract NG is ERC721Connector {
 
     // txnhash -> tokenid
     mapping(string => uint256) private _txnHashToTokenId;
+
+    // 브랜드 계정 주소로 등록된 품목 리스트 검색
+    function getAddressToCategorys(address caller)
+        public
+        view
+        returns (Product[] memory)
+    {
+        // return brandToCategory[msg.sender];
+        return brandToProduct[caller];
+    }
+
+    // 브랜드 계정 주소 및 품목 번호로 품목의 index 검색
+    function getCategoryIndex(address caller, string memory productNo)
+        public
+        view
+        returns (uint256)
+    {
+        return ProductToIndex[caller][productNo];
+    }
+
+    // 브랜드 계정 주소 및 품목 index로 인증서 목록 검색
+    function getProductidxToNgs(address caller, uint256 categoryIdx)
+        public
+        view
+        returns (NGInfo[] memory)
+    {
+        return ProductToNg[caller][categoryIdx];
+    }
+
+    // 브랜드 계정 주소 및 품목 번호로 인증서 목록 검색
+    function getProductnoToNgs(address caller, string memory productNo)
+        public
+        view
+        returns (NGInfo[] memory)
+    {
+        uint256 idx = getCategoryIndex(caller, productNo);
+        return ProductToNg[caller][idx];
+    }
 
     //ng) input: txnhash, output: tokenId
     function getTokenIdFromTxnHash(string memory _txnHash)
@@ -115,24 +166,60 @@ contract NG is ERC721Connector {
         _accountsAuth[_address] = 2;
     }
 
-    modifier ngExists(
+    modifier productExists(
         string memory _brandNm,
+        string memory _productName,
         string memory _productNo,
-        string memory _serialNo,
         string memory _mfd,
-        string memory _madeIn
+        string memory _madeIn,
+        string memory _price
     ) {
         bool check = false;
-        for (uint256 i = 0; i < ngs.length; i++) {
+        for (uint256 i = 0; i < brandToProduct[msg.sender].length; i++) {
             if (
-                keccak256(bytes(ngs[i].brandNm)) ==
+                keccak256(bytes(brandToProduct[msg.sender][i].brandNm)) ==
                 keccak256(bytes(_brandNm)) &&
-                keccak256(bytes(ngs[i].productNo)) ==
+                keccak256(bytes(brandToProduct[msg.sender][i].productName)) ==
+                keccak256(bytes(_productName)) &&
+                keccak256(bytes(brandToProduct[msg.sender][i].productNo)) ==
                 keccak256(bytes(_productNo)) &&
-                keccak256(bytes(ngs[i].serialNo)) ==
-                keccak256(bytes(_serialNo)) &&
-                keccak256(bytes(ngs[i].mfd)) == keccak256(bytes(_mfd)) &&
-                keccak256(bytes(ngs[i].madeIn)) == keccak256(bytes(_madeIn))
+                keccak256(bytes(brandToProduct[msg.sender][i].mfd)) ==
+                keccak256(bytes(_mfd)) &&
+                keccak256(bytes(brandToProduct[msg.sender][i].madeIn)) ==
+                keccak256(bytes(_madeIn)) &&
+                keccak256(bytes(brandToProduct[msg.sender][i].price)) ==
+                keccak256(bytes(_price))
+            ) {
+                check = true;
+            }
+        }
+        require(!check, "Error Category already exists");
+        _;
+    }
+
+    modifier ngExists(string memory _productNo, string memory _serialNo) {
+        bool check = false;
+        bool hasProduct = false;
+        // 해당 품목 번호가 존재해야함
+        Product[] memory cates = getAddressToCategorys(msg.sender);
+        for (uint256 i = 0; i < cates.length; i++) {
+            if (
+                keccak256(bytes(cates[i].productNo)) ==
+                keccak256(bytes(_productNo))
+            ) {
+                hasProduct = true;
+            }
+        }
+
+        uint256 idx = ProductToIndex[msg.sender][_productNo];
+        for (uint256 i = 0; i < ProductToNg[msg.sender][idx].length; i++) {
+            if (
+                // serialNo는 고유해야함 : NFT로써 유일성을 가지게 되는 핵심 정보
+                // 해당 브랜드 해당 품목에서 시리얼번호는 중복되어서는 안됨
+                keccak256(bytes(brandToProduct[msg.sender][idx].productNo)) ==
+                keccak256(bytes(_productNo)) &&
+                keccak256(bytes(ProductToNg[msg.sender][idx][i].serialNo)) ==
+                keccak256(bytes(_serialNo))
             ) {
                 check = true;
             }
@@ -165,47 +252,56 @@ contract NG is ERC721Connector {
         addTokenHistory(_tokenId);
     }
 
-    function mint(
+    function addProduct(
         string memory _brandNm,
+        string memory _productName,
         string memory _productNo,
-        string memory _serialNo,
         string memory _mfd,
-        string memory _madeIn
-    ) public ngExists(_brandNm, _productNo, _serialNo, _mfd, _madeIn) {
-        NGInfo memory _ngInfo = NGInfo(
+        string memory _madeIn,
+        string memory _price
+    )
+        public
+        productExists(_brandNm, _productName, _productNo, _mfd, _madeIn, _price)
+    {
+        Product memory category = Product(
             _brandNm,
+            _productName,
             _productNo,
-            _serialNo,
             _mfd,
-            _madeIn
+            _madeIn,
+            _price
         );
-        // require(ngs[_ngInfo]!='', 'Error _ngInfo already exists');
-        // require(!_ngExists[_ngInfo], 'Error _ngInfo already exists');
+
+        ProductToIndex[msg.sender][_productNo] = brandToProduct[msg.sender]
+            .length;
+
+        brandToProduct[msg.sender].push(category);
+    }
+
+    function mint(string memory _productNo, string memory _serialNo)
+        public
+        ngExists(_productNo, _serialNo)
+    {
+        uint256 idx = getCategoryIndex(msg.sender, _productNo);
+        Product[] memory categotys = getAddressToCategorys(msg.sender);
+        NGInfo memory _ngInfo = NGInfo(_serialNo, categotys[idx]);
+
+        ProductToNg[msg.sender][idx].push(_ngInfo);
+        ngs.push(_ngInfo);
+        blockNos.push(block.number);
+        uint256 _id = ngs.length - 1;
+
         // mint는 계정권한에 1 또는 2로 등록되어있는 계정만 할 수 있음
         require(
             _accountsAuth[msg.sender] != 0,
             "NG : mint requests can only registered accounts for company"
         );
 
-        ngs.push(_ngInfo);
-        blockNos.push(block.number);
-        uint256 _id = ngs.length - 1;
         _mint(msg.sender, _id);
-
         _mintedTokens[msg.sender].push(_id);
         addTokenHistory(_id);
         // _ngExists[_ngInfo] = true;
     }
-
-    // function mint(string memory _ng) public {
-    //     require(!_ngExists[_ng], 'Error -ng already exists');
-
-    //     ngs.push(_ng);
-    //     uint256 _id = ngs.length - 1;
-    //     _mint(msg.sender, _id);
-
-    //     _ngExists[_ng] = true;
-    // }
 
     constructor(string memory named, string memory symbolified)
         ERC721Connector(named, symbolified)
