361103f60800f471d922bd3d657c6d115f464e98
KimDaeyeong <daeyoung4088@hanmail.net>
2022-09-29 02:35:44 +0900

    [style] aboutus페이지 디자인중

--- frontend/src/features/main/aboutus/aboutus.js
+++ frontend/src/features/main/aboutus/aboutus.js
@@ -1,17 +1,36 @@
 import NavBar from "../../../common/navbar/NavBar"
 import Footer from "../../../common/footer/Footer"
+import { useEffect, useReducer } from "react"
+import "./aboutus.css"
 
 function AboutUs() {
 
+    // const section1 = useRef(null);
+    // const section2 = useRef(null);
+    // const section3 = useRef(null);
+
+    useEffect(() => {
+        var video = document.querySelector('#video');
+        if (video.paused) {
+            video.play();
+        }
+    }, []);
+
+    const goSection2 = () => {
+        const el = document.getElementById("sec-2")
+        el.classList.add('fadeInUp')
+    }
+    
+
     return(
-        <div>
+        <div className="about-us">
         <NavBar/>
-            <div
-            className='p-5 text-center bg-image mt-5'
-            style={{ height: '300px', backgroundColor: 'rgba(0, 0, 0, 0.3)' }}
-            >
-                <div>
-                    <h1> 진품 인증에 관심이 많은 ~~~ 만들었다 ~~~~</h1>
+            <div className='section-1'>
+                <video autoPlay muted loop id="video">
+                    <source src={ require('../../../assets/img/BGV1.mp4') } type="video/mp4" />
+                </video>
+                <div className="content-1 text-center">
+                    <h1>소비자의 권리와 제품의 가치를 끌어올리기 위해</h1>
                 </div>
             </div>
             <div
@@ -19,7 +38,15 @@ function AboutUs() {
             style={{ height: '400px', backgroundColor: 'rgba(0, 0, 0, 0)' }}
             >
                 <div>
-                    <h1> 우리의 미션 우리의 비전 우리의 가치</h1>
+                    <h1> 우리의 미션 - 블록체인 솔루션을 통해서 투명성과 추적성을 제공합니다.
+                         명품의 선순환적인 거래 및 위조방지를 위해 기술을 제공합니다.
+
+                         우리의 비전 - 소비자 및 기업의 신뢰성과 진정성 보장을 가장 중요하게 생각합니다.
+                         위조 및 회색시장 방지를 위해, 선순환적인 경제활동을 장려하기 위해 노력합니다.
+                         
+                         우리의 가치 - 고객 경험에 새로운 패러다임을 제공
+                         무한한 확장 가능성, 브랜드/물건
+                    </h1>
                 </div>
             </div>
             <div
