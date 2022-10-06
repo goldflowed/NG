f58b8a297298c6a26399ec62c5b42b340b956646
김유리 <goldflowed@gmail.com>
2022-10-07 06:39:24 +0900

    [feat] fix ca

--- frontend/src/common/api/http-common.js
+++ frontend/src/common/api/http-common.js
@@ -1,8 +1,8 @@
 import baseAxios from 'axios';
 
 const axios = baseAxios.create({
-  baseURL: 'https://j7e206.p.ssafy.io/api/v1',
-  // baseURL: 'http://localhost:8080/api/v1',
+  // baseURL: 'https://j7e206.p.ssafy.io/api/v1',
+  baseURL: 'http://localhost:8080/api/v1',
   // headers: {
   //   'Content-Type': 'application/json',
   // },
--- frontend/src/common/web3/web3Config.js
+++ frontend/src/common/web3/web3Config.js
@@ -3,7 +3,8 @@ import NG from './abi/NG.json'
 
 const nftAbi = NG.abi
 
-export const nftCA = `0x68bB4400b254588f8E2024601Ca8AcBB929f82cd`;
+// export const nftCA = `0x68bB4400b254588f8E2024601Ca8AcBB929f82cd`;
+export const nftCA = `0x556A8305576C7aDb9FA7F5F0930b2DF3b49af8b9`;
 
 export const web3 = new Web3(window.ethereum);
 
